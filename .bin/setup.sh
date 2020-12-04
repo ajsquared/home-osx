5d4ccf3c8fe86ab70e3fe28a27575ec2b77321b8
Andrew Johnson <andrew@andrewjamesjohnson.com>
2020-12-04 18:05:14 -0500

    clean up a bunch of unused stuff

--- .bin/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-echo 'Installing oh-my-zsh'
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-echo 'Installing TPM'
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
