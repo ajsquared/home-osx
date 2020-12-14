4013d71e94f598d5edc6ee3a5d9b4df3257c40f6
Andrew Johnson <andrew@andrewjamesjohnson.com>
2020-12-13 19:28:34 -0500

    fix maximizing frame

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,6 +1,7 @@
 ;;; Reduce GC frequency during startup
 (setq gc-cons-threshold (* 50 1000 1000))
 
+;;; Log startup stats
 (add-hook 'emacs-startup-hook
           (lambda ()
             (message "Emacs ready in %s with %d garbage collections."
