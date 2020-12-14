8feea0ab0e222f2793a1159159f7beaea78f616b
Andrew Johnson <andrew@andrewjamesjohnson.com>
2020-12-13 19:13:13 -0500

    move emacs config to org-babel

---
+++ .emacs.d/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(company helm-company spaceline diminish perspective terraform auto-package-update use-package doom-themes blacken all-the-icons neotree yasnippet git-commit hcl-mode helm helm-core markdown-mode projectile scala-mode yaml-mode thrift terraform-mode rg rainbow-delimiters python-mode puppet-mode protobuf-mode powerline magit json-mode helm-projectile helm-descbinds helm-xref dumb-jump bazel-mode bar-cursor autopair auto-virtualenvwrapper auctex)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ .emacs.d/early-init.el
@@ -0,0 +1,10 @@
+;;; Reduce GC frequency during startup
+(setq gc-cons-threshold (* 50 1000 1000))
+
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "Emacs ready in %s with %d garbage collections."
+                     (format "%.2f seconds"
+                             (float-time
+                              (time-subtract after-init-time before-init-time)))
+                     gcs-done)))
